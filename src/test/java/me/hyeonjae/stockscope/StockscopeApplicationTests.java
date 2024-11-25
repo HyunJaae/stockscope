add4d60b44af58fe15985ab6c5995f16bcd57ea0
Devlee <hjcloud@me.com>
2024-11-25 15:33:08 +0900

    Chore(project): 초기 프로젝트 설정
    - Spring Boot 프로젝트 생성
    - 기본 의존성 추가
    - gitignore 설정
    
    resolves: #1

---
+++ src/test/java/me/hyeonjae/stockscope/StockscopeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyeonjae.stockscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockscopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
